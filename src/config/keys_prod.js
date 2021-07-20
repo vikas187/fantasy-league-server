07b8c8824c404f58654cc3fb939a757663b2fd2d
VIKABHAN <VIKABHAN@VIKABHAN-7490.in.oracle.com>
2021-07-21 00:27:43 +0530

    keys update

--- src/config/keys_prod.js
+++ src/config/keys_prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://comet-user:hello@123@cluster0.ydv8r.mongodb.net/comet-chat-db?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://comet-user:hello@123@cluster0.ydv8r.mongodb.net/dream-11?retryWrites=true&w=majority'
 }
